cf22baf51999bbc0e698ebdf1a662f932b6d9c77
Daniel González Jiménez <danielgj@MacBook-Pro-de-Daniel.local>
2023-01-24 17:20:34 -0600

    Prueba respuesta 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,13 @@ import { Component, HostListener } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
+  data: string = "Campo"
   @HostListener('window:message', ['$event']) onPostMessage(event: any) {
     console.log("Evento")
     console.log(event)
     if (event.data.data) {
       alert("Vamos")
+      this.data = event.data.data
       alert(JSON.stringify(event.data.data))
     } else {
       alert("No vamos")

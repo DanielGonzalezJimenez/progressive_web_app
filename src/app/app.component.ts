9f34e9160c3413b7d2f5774d416aba19b4afd3d5
Daniel González Jiménez <danielgj@MacBook-Pro-de-Daniel.local>
2023-01-25 13:00:33 -0600

    Prueba 7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,9 +16,9 @@ export class AppComponent {
     this.heroService.log("window:message - " + JSON.stringify(event, null, 4))
     this.heroService.log("data - " + JSON.stringify(event.data, null, 4))
 
-    event.origin.postMessage("hi there yourself!  the secret response " +
+    event.source.postMessage("hi there yourself!  the secret response " +
                            "is: rheeeeet!",
-                           event.source);
+                           event.origin);
   }
   
 }
